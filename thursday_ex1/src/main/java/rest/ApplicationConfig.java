e8ebd61ae1884c721fba04a6ec89e75671480e72
sslhansen <sebastian@lundby.net>
2020-11-07 15:53:23 +0100

    changing some things

--- thursday_ex1/src/main/java/rest/ApplicationConfig.java
+++ thursday_ex1/src/main/java/rest/ApplicationConfig.java
@@ -9,7 +9,7 @@ import javax.ws.rs.core.Application;
  */
 @javax.ws.rs.ApplicationPath("api")
 public class ApplicationConfig extends Application {
-
+/////////////
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
